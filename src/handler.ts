577b9b3579d09d08ef30fb58cd9232ab9a9129d7
Ludovic <duranteau.l.i@gmail.com>
2023-12-14 11:29:26 +0000

    feature (handler) : add serverless with proxy

---
+++ src/handler.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+import { AppModule } from './app.module';
+
+let serverlessApp: Handler;
+
+export const app = async (event: any, context: Context, callback: Callback) => {
+  if (!serverlessApp) {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors();
+    await app.init();
+
+    const expressApp = app.getHttpAdapter().getInstance();
+    serverlessApp = serverlessExpress({ app: expressApp });
+  }
+
+  return serverlessApp(event, context, callback);
+};
